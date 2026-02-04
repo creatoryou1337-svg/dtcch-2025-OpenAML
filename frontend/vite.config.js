862e1f9cdcbff67b74b7fb3bc69278d176e7eaab
creatoryou1337-svg <creatoryou1337@gmail.com>
2026-02-04 14:44:22 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // для Render
+  base: '/',  // обязательно для Render и GitHub Pages
   server: {
     proxy: {
       '/api': {
-        target: 'https://dtcch-2025-openaml.onrender.com', // твой бэкенд URL
+        target: 'https://dtcch-2025-openaml.onrender.com',  // твой бэкенд URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
